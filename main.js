120318a2fac34dec84a6463447223d98d3f9d667
0gzs <r.jesus9107@gmail.com>
2024-07-23 11:55:05 -0700

    Add `addTask()` and `cancelTask()`: - addTask appends a form to the task list to create a new task - cancelTask closes the form and cancels the task creation

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 1, title: 'Start Todo List', description: 'Final Project', dueDate: '2024-22-07', priority: 'high' },
         // ....
     ]
-    
+
     function renderTasks() {
         const taskList = document.getElementById('allTasks')
         taskList.innerHTML = ""
-    
+        
         tasks.forEach(task => {
             const li = document.createElement('li')
-    
+            
             li.draggable = true
             li.id = task.id
             li.innerHTML = `
@@ -19,11 +19,36 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p>${task.dueDate}</p>
                 <p>${task.priority}</p>
                 <button>Edit Task</button>
-                <button onClick="alert('Hi')">Delete</delete>
+                <button onclick="alert('Hi')">Delete</delete>
             `
             taskList.appendChild(li)
         })
     }
-    
+
     renderTasks()
 })
+
+// Add task to populate form to then 'saveTask'
+function addTask() {
+    const taskList = document.getElementById('allTasks')
+    const taskForm = document.createElement('li')
+
+    taskForm.id = 'taskForm'
+    taskForm.innerHTML = `
+            <input type='text' placeholder="Title" />
+            <input type='text' placeholder='Description' />
+            <div><input id='priority-low' type='radio' name="priority" value="low" />!</div>
+            <div><input id='priority-medium' type='radio' name="priority" value="medium" />!!</div>
+            <div><input id='priority-high' type='radio' name="priority" value="high" />!!!</div>
+            <button onclick="alert('saveTask()')">Save</button>
+            <button onclick="cancelTask()">Cancel</button>
+        `
+
+    taskList.appendChild(taskForm)
+}
+
+function cancelTask() {
+    const taskForm = document.getElementById('taskForm')
+
+    if (taskForm) taskForm.remove()
+}
